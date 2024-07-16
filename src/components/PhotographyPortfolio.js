3a9ff2ec53c82c5cf7bff643b3bfaad76a024614
Jdka1 <staryan.mehra@gmail.com>
2024-07-15 18:38:28 -0700

    css

--- src/components/PhotographyPortfolio.js
+++ src/components/PhotographyPortfolio.js
@@ -1,30 +1,48 @@
-// src/PhotoPortfolio.js
-import React from 'react';
+import React, { useState } from 'react';
 import './PhotographyPortfolio.css';
 
 const photos = [
-    '4V7A0096.jpg',
-    '4V7A0138-Edit.jpg',
-    '4V7A0770.jpg',
-    '4V7A1319.jpg',
-    '4V7A2066.jpg',
-    '4V7A3643.jpg',
-    '4V7A3683.jpg',
-    '4V7A3686.jpg',
-    '4V7A5823-Edit-Edit.jpg',
-    '4V7A5833-Enhanced-NR-Edit.jpg',
-    '4V7A6203.jpg',
-    '4V7A6236.jpg',
-    '4V7A6479-Edit.jpg',
-  ];
-  
+  '4V7A0096.jpg',
+  '4V7A0138-Edit.jpg',
+  '4V7A0770.jpg',
+  '4V7A1319.jpg',
+  '4V7A2066.jpg',
+  '4V7A3643.jpg',
+  '4V7A3683.jpg',
+  '4V7A3686.jpg',
+  '4V7A5823-Edit-Edit.jpg',
+  '4V7A5833-Enhanced-NR-Edit.jpg',
+  '4V7A6203.jpg',
+  '4V7A6236.jpg',
+  '4V7A6479-Edit.jpg',
+];
 
 const PhotoPortfolio = () => {
+  const [fullScreenPhoto, setFullScreenPhoto] = useState(null);
+  const [noTransition, setNoTransition] = useState(false);
+
+  const handlePhotoClick = (index) => {
+    if (fullScreenPhoto === index) {
+      setFullScreenPhoto(null);
+      setNoTransition(true);
+      setTimeout(() => {
+        setNoTransition(false);
+      }, 1)
+    } else {
+      setFullScreenPhoto(index);
+    }
+  };
+
   return (
     <div className="photo-grid">
       {photos.map((photo, index) => (
         <div className="photo-item" key={index}>
-        <img src={`${process.env.PUBLIC_URL}/photography/${photo}`} alt={`Photo ${index + 1}`} />
+          <img
+            src={`${process.env.PUBLIC_URL}/photography/${photo}`}
+            alt={`Photo ${index + 1}`}
+            className={`${fullScreenPhoto === index ? 'fullscreen' : ''} ${noTransition ? 'no-transition' : ''}`}
+            onClick={() => handlePhotoClick(index)}
+          />
         </div>
       ))}
     </div>
